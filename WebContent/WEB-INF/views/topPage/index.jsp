1e57c6bb20326295584ce8fdd8f5cb5479f00b34
kodai.murakami <hakmd.m.98@gmail.com>
2021-09-04 20:27:04 +0900

    Modify TopPage

--- WebContent/WEB-INF/views/topPage/index.jsp
+++ WebContent/WEB-INF/views/topPage/index.jsp
@@ -1,7 +1,37 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" import="models.Calendars" %>
+<% Calendars cls=(Calendars)request.getAttribute("cls"); %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:import url="../layout/app.jsp">
     <c:param name="content">
         <h2>今日も勉強おつかれさまでした！</h2>
+         <div id="container">
+    <h1><%=cls.getYear() %>年<%=cls.getMonth() %>月のカレンダー</h1>
+    <p>
+		　<a href="?year=<%=cls.getYear()%>&month=<%=cls.getMonth()-1%>">前月</a>
+    	<a href="?year=<%=cls.getYear()%>&month=<%=cls.getMonth()+1%>">翌月</a>
+    </p>
+    <table>
+      <tr>
+        <th>日</th>
+        <th>月</th>
+        <th>火</th>
+        <th>水</th>
+        <th>木</th>
+        <th>金</th>
+        <th>土</th>
+      </tr>
+      <%for(String[] row: cls.getDate()){ %>
+      <tr>
+      	<%for(String col:row) {%>
+      		<%if (col.startsWith("*")){ %>
+      			<td class="today"><%=col.substring(1)%></td>
+      		<%}else{ %>
+      			<td><%=col %></td>
+      		<%} %>
+      	<%} %>
+      </tr>
+      <%} %>
+    </table>
+  </div><!-- end container-->
     </c:param>
 </c:import>
--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.CalendarLogic;
+import models.Calendars;
+
 /**
  * Servlet implementation class TopPageIndexServlet
  */
@@ -27,6 +30,31 @@ public class TopPageIndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String s_year=request.getParameter("year");
+		String s_month=request.getParameter("month");
+		CalendarLogic logic=new CalendarLogic();
+		Calendars cls=null;
+		if(s_year != null && s_month != null) {
+			int year =Integer.parseInt(s_year);
+			int month=Integer.parseInt(s_month);
+			if(month==0) {
+				month=12;
+				year--;
+			}
+			if(month==13) {
+				month=1;
+				year++;
+			}
+			//年と月のクエリパラメーターが来ている場合にはその年月でカレンダーを生成する
+			cls=logic.createMyCalendar(year,month);
+		}else {
+			//クエリパラメータが来ていないときは実行日時のカレンダーを生成する。
+			cls=logic.createMyCalendar();
+		}
+		//リクエストスコープに格納
+		request.setAttribute("cls", cls);
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
 		rd.forward(request, response);
 	}
